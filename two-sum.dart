ed27fd1729f73752c0feeb562155801441df5b91
Md Mahbub Hasan Talukder <mahbub.hasan@brainstation-23.com>
2024-04-30 12:03:42 +0600

    Two sum problem solution added in Dart

--- two-sum.dart
+++ two-sum.dart
@@ -1,1 +1,21 @@
-abc
+/*
+Problem: Two sum problem
+Problem link: https://leetcode.com/problems/two-sum
+*/
+
+class Solution {
+ List<int> twoSum(List<int> nums, int target) {
+    bool flag = false;
+    List<int>ans = [];
+    for(int i=0;i<nums.length;i++){
+      int cur = nums[i];
+      if(nums.indexOf(target-cur)!=i){
+        flag = true;
+        ans.add(i);
+        ans.add(nums.indexOf(target-cur));
+        break;
+      }
+    }
+    return ans;
+  }
+}
