6814a51d95c5ed0bab855fe4ef99841636b418de
Md Mahbub Hasan Talukder <mahbub.hasan@brainstation-23.com>
2024-05-02 11:03:39 +0600

    Optimized solution, beats 96% using Dart Language

--- merge_intervals.dart
+++ merge_intervals.dart
@@ -1,12 +1,15 @@
+import 'dart:math';
+
 class Solution {
   List<List<int>> merge(List<List<int>> intervals) {
-    List<List<int>> ans = [];
+     intervals.sort((a, b) => a[0].compareTo(b[0])); 
+     List<List<int>> ans = [];
     int st = intervals[0][0];
     int ed = intervals[0][1];
     if(intervals.length == 1)return intervals;
     for(int i=1;i<intervals.length;i++){
         int st1 = intervals[i][0];
-        int ed1 = intervals[i][1];
+        int ed1 = max(intervals[i][1],ed);
         if(st1<=ed)ed = ed1;
         else{
             ans.add([st,ed]);
